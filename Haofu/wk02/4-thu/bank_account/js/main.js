a6f05861d150ed7332645900e85e9c4d2a6e7306
Haofu <haofu.zhu@gmail.com>
2017-12-07 23:01:03 +1100

    improve the solution using object

--- Haofu/wk02/4-thu/bank_account/js/main.js
+++ Haofu/wk02/4-thu/bank_account/js/main.js
@@ -12,110 +12,116 @@ const checkingBalanceSpan = checkingBox.querySelector('.balance').querySelector(
 const savingInputBox = savingBox.querySelector('input');
 const checkingInputBox = checkingBox.querySelector('input');
 
-let backAccount = {
+let bankAccount = {
   savingBalance: 0,
   checkingBalance: 0,
-  withdrawFromSaving: (amount) => {
-    if (true) {
-
+  withdrawFromSaving: function(amount) {
+    if (amount<=this.savingBalance) {
+      this.savingBalance -= amount;
+      return this.savingBalance;
+    } else if (amount<=(this.savingBalance+this.checkingBalance)) {
+      let exceed = amount-this.savingBalance;
+      this.savingBalance = 0;
+      this.withdrawFromChecking(exceed);
+      return -1;
     } else {
-
+      return 0;
     }
   },
-  withdrawFromChecking: () => {
-
+  withdrawFromChecking: function(amount) {
+    if (amount<=this.checkingBalance) {
+      this.checkingBalance -= amount;
+      return this.checkingBalance;
+    } else {
+      return 0;
+    }
+  },
+  depositToSaving: function(amount) {
+    this.savingBalance += amount;
+    return this.savingBalance;
+  },
+  depositToChecking: function(amount) {
+    this.checkingBalance += amount;
+    return this.checkingBalance;
   }
-}
-
-const init = () => {
-  
 }
 
 savingWithdrawBtn.addEventListener('click', (e) => {
-  let current = Number(savingBalanceSpan.textContent);
   let withdrawAmount = Number(savingInputBox.value);
-  if (withdrawAmount) {
-    if (withdrawAmount>0&&withdrawAmount<=current) {
-      current -= withdrawAmount;
-      savingBalanceSpan.textContent = current.toFixed(2);
-    } else if (withdrawAmount>current) {
-      alert("Insufficient Money");
+  if (withdrawAmount && withdrawAmount>0) {
+    if (bankAccount.withdrawFromSaving(withdrawAmount)) {
+      updateBalance();
     } else {
-      alert("Please put a positive number to withdraw");
+      alert("Insufficient Money");
     }
   } else {
-
+    alert("Please put a positive number to withdraw");
   }
   savingInputBox.value = "";
-  checkAccountBalance();
+  checkBalance();
 });
 
 savingDepositBtn.addEventListener('click', (e) => {
-  let current = Number(savingBalanceSpan.textContent);
   let depositAmount = Number(savingInputBox.value);
-  if (depositAmount) {
-    if (depositAmount>0) {
-      current += depositAmount;
-      savingBalanceSpan.textContent = current.toFixed(2);
-    } else {
-      alert("Please put a positive number to deposit");
-    }
+  if (depositAmount && depositAmount>0) {
+    bankAccount.depositToSaving(depositAmount);
+    updateBalance();
   } else {
-
+    alert("Please put a positive number to deposit");
   }
   savingInputBox.value = "";
-  checkAccountBalance()
+  checkBalance();
 });
 
-const checkAccountBalance = () => {
-  let savingCurrent = Number(savingBalanceSpan.textContent);
-  let checkingCurrent = Number(checkingBalanceSpan.textContent);
-  if (savingCurrent<=0) {
+checkingWithdrawBtn.addEventListener('click', (e) => {
+  let withdrawAmount = Number(checkingInputBox.value);
+  if (withdrawAmount && withdrawAmount>0) {
+    if (bankAccount.withdrawFromChecking(withdrawAmount)) {
+      updateBalance();
+    } else {
+      alert("Insufficient Money");
+    }
+  } else {
+    alert("Please put a positive number to withdraw");
+  }
+  checkingInputBox.value = "";
+  checkBalance();
+});
+
+checkingDepositBtn.addEventListener('click', (e) => {
+  let depositAmount = Number(checkingInputBox.value);
+  if (depositAmount && depositAmount>0) {
+    bankAccount.depositToChecking(depositAmount);
+    updateBalance();
+  } else {
+    alert("Please put a positive number to deposit");
+  }
+  checkingInputBox.value = "";
+  checkBalance();
+});
+
+const checkBalance = () => {
+  let saving = bankAccount.savingBalance;
+  let checking = bankAccount.checkingBalance;
+  if (saving<=0) {
     savingBalanceSpan.setAttribute("style", "color: red");
   } else {
     savingBalanceSpan.setAttribute("style", "color: gray");
   }
-  if (checkingCurrent<=0) {
+  if (checking<=0) {
     checkingBalanceSpan.setAttribute("style", "color: red");
   } else {
     checkingBalanceSpan.setAttribute("style", "color: gray");
   }
 }
 
-checkingWithdrawBtn.addEventListener('click', (e) => {
-  let current = Number(checkingBalanceSpan.textContent);
-  let withdrawAmount = Number(checkingInputBox.value);
-  if (withdrawAmount) {
-    if (withdrawAmount>0&&withdrawAmount<=current) {
-      current -= withdrawAmount;
-      checkingBalanceSpan.textContent = current.toFixed(2);
-    } else if (withdrawAmount>current) {
-      alert("Insufficient Money");
-    } else {
-      alert("Please put a positive number to withdraw");
-    }
-  } else {
+const updateBalance = () => {
+  savingBalanceSpan.textContent = bankAccount.savingBalance.toFixed(2);
+  checkingBalanceSpan.textContent = bankAccount.checkingBalance.toFixed(2);
+}
 
-  }
-  checkingInputBox.value = "";
-  checkAccountBalance();
-});
+const init = () => {
+  checkBalance();
+}
 
-checkingDepositBtn.addEventListener('click', (e) => {
-  let current = Number(checkingBalanceSpan.textContent);
-  let depositAmount = Number(checkingInputBox.value);
-  if (depositAmount) {
-    if (depositAmount>0) {
-      current += depositAmount;
-      checkingBalanceSpan.textContent = current.toFixed(2);
-    } else {
-      alert("Please put a positive number to deposit");
-    }
-  } else {
-
-  }
-  checkingInputBox.value = "";
-  checkAccountBalance()
-});
-
-checkAccountBalance();
+init();
