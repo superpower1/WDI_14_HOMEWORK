78787465aca6a6ae8a198896d9b6fddbc6c2f779
Haofu <haofu.zhu@gmail.com>
2018-01-09 16:32:32 +1100

    add function to handle movie not found and poster not found

--- Haofu/wk05/1-mon/movie/main.rb
+++ Haofu/wk05/1-mon/movie/main.rb
@@ -9,13 +9,41 @@ end
 get '/movie' do
   movie_name = params[:movie_name]
   result = HTTParty.get("http://www.omdbapi.com/?apikey=2f6435d9&t=#{movie_name}").parsed_response
-  @info = result.select do |key, value|
-    ["Title", "Year", "Rated", "Runtime", "Director"].include?(key)
+
+  def render_not_found
+    erb :not_found
   end
-  @imgSrc = result["Poster"]
-  @actors = result["Actors"].split(',')
-  @tomato = result["Ratings"].find{|rating| rating["Source"] == "Rotten Tomatoes"}["Value"]
-  erb :movie
+
+  def render_found(result)
+    @info = result.select do |key, value|
+      ["Title", "Year", "Rated", "Runtime", "Director"].include?(key)
+    end
+
+    @isPoster = true
+
+    if result["Poster"]=="N/A"
+      @isPoster = false
+    else
+      @imgSrc = result["Poster"]
+    end
+
+    @actors = result["Actors"].split(',')
+
+    if t=result["Ratings"].find{|rating| rating["Source"] == "Rotten Tomatoes"}
+      @tomato = t["Value"]
+    else
+      @tomato = "N/A"
+    end
+
+    erb :movie
+  end
+
+  if result["Response"] == "False"
+    render_not_found
+  else
+    render_found(result)
+  end
+
 end
 
 get '/about' do
--- Haofu/wk05/1-mon/movie/public/javascripts/application.js
+++ Haofu/wk05/1-mon/movie/public/javascripts/application.js
@@ -1,1 +1,10 @@
-
+const button = document.querySelector('button');
+const searchInput = document.querySelector('.search-input');
+searchInput.addEventListener('keyup', e=>{
+  console.log(e.target.value);
+  if (e.target.value === "") {
+    button.classList.add('hidden');
+  } else {
+    button.classList.remove('hidden');
+  }
+});
