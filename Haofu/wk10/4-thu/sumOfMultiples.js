bdb44959c2faaa8f61107f8a8b730d8783ced738
Haofu <haofu.zhu@gmail.com>
2018-02-16 09:23:46 +1100

    add homework of sumOfMultiples

---
+++ Haofu/wk10/4-thu/sumOfMultiples.js
@@ -0,0 +1,12 @@
+const sumOfMultiples = num => {
+  let sum = 0;
+  for (let i = 0; i < num; i++) {
+    if (i%3===0||i%5===0) {
+      sum += i
+    }
+  }
+  return sum;
+}
+
+const num = process.argv[2];
+console.log(sumOfMultiples(num));
